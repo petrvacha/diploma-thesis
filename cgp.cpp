9c8efd64751619b8f4dcb4035e1140e9adf7edf5
Petr Vácha <pan.vacha@gmail.com>
2012-04-27 17:38:07 +0200

    kostra programu

---
+++ cgp.cpp
@@ -0,0 +1,41 @@
+/*
+ * Author: Petr Vacha
+ * Email: xvacha03@stud.fit.vutbr.cz
+ * Program: CGP
+ * Datum 2012
+ */
+
+
+/*
+ * Nacteni vstupnich dat
+ */
+void readData()
+{
+	
+}
+
+/*
+ * Ohodnoceni populace
+ */
+inline int fitness()
+{
+
+}
+
+/*
+ * Mutace na populaci
+ */
+inline void mutace()
+{
+}
+
+
+
+/*
+ * Hlavni funkce
+ */
+int main(int argc, char* argv[])
+{
+
+	return 0;
+}
